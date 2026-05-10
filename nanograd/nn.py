22dc61aa132a4e74d829c8deae56372352022ded
Shubham Malhotra <shubham.malhotra28@gmail.com>
2026-05-09 23:57:09 -0700

    mlp layer neuron all done

--- nanograd/nn.py
+++ nanograd/nn.py
@@ -4,7 +4,7 @@ from nanograd.engine import Value
 class Module:
     
     def zero_grad(self):
-        for p in self.parameter():
+        for p in self.parameters():
             p.grad = 0
         
     def parameters(self):
@@ -24,10 +24,35 @@ class Neuron(Module):
     
     def parameters(self):
         return self.w + [self.b]
-    
     def __repr__(self):
         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
+class Layer(Module):
 
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+    
+    def __Call__(self, x):
+        out = [n(x) for x in self.neurons]
+        return out[0] if len(out) == 1 else out
+    
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+    
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+    
+class MLP(Module):
 
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
 
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
